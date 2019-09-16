95f7c4099d8bfdba0e5bc3f16dc3cb6747b37976
Chad Burkins <chad.burkins@gmail.com>
2019-09-16 08:51:36 -0400

    replaced with my own template

---
+++ src/components/app.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class App extends React.Component {
+    state = {
+        name: ""
+    };
+
+    render() {
+        return (
+            <div className="ui container" style={{ marginTop: "10px" }}>
+                App
+            </div>
+        );
+    }
+}
+
+export default App;
